73ca3d3332b2a45fe49b39f7827423b1515c75dc
thomson209 <doug.thomson@keyin.com>
2021-11-28 17:35:07 -0330

    Added unit testing dependencies

---
+++ src/test/java/com/example/accessingdatamysql/ApplicationTest.java
@@ -0,0 +1,28 @@
+/*package com.example.accessingdatamysql;
+
+import org.assertj.core.api.Assert;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTest {
+
+
+	private AccessingDataMysqlApplication accessingDataMysqlApplication = new AccessingDataMysqlApplication();
+
+	@Test
+	public void testResultIsFalseDefault() {
+		boolean returnValue = accessingDataMysqlApplication.isTrue();
+		Assert.assertFalse(returnValue);
+	}
+
+	@Test
+	public void testResultIsTrueAfterSetting() {
+		boolean returnValue = accessingDataMysqlApplication.isTrue();
+		Assert.assertFalse(returnValue);
+
+
+		accessingDataMysqlApplication.setValue(true);
+		returnValue = accessingDataMysqlApplication.isTrue();
+		Assert.assertTrue(returnValue);
+}*/
